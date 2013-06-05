db012028f9d6c9d6305709128338cd60d127c2a8
Jorge Hernandez <wexic@hotmail.com>
2013-06-05 23:26:27 +0300

    Basic Java example using Maven

---
+++ JavaApp_Maven/src/main/java/com/kartonki/javaapp_maven/App.java
@@ -0,0 +1,13 @@
+package com.kartonki.javaapp_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
